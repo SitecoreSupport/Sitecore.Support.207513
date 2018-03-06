b4eb0d2a6f090e74181ec1d1772775ff84fa4e81
Paul Stupka <psk@sitecore.net>
2018-03-06 16:00:59 +0200

    Sitecore implementation

---
+++ src/Sitecore.Support.207513/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.207513")]
+[assembly: AssemblyProduct("Sitecore.Support.207513")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Content Tree search may not work when using Azure Search provider")]
+
